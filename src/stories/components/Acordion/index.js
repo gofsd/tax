218c60ba8d1f88f993d8adb769f68868ac5ec084
Alexandr Zhuk <sanek201dg@ukr.net>
2019-05-16 22:41:16 +0300

    refactor code

--- src/stories/components/Acordion/index.js
+++ src/stories/components/Acordion/index.js
@@ -16,7 +16,8 @@ import {
     Right,
     Left,
 } from "native-base";
-import {StyleSheet} from "react-native";
+// import {StyleSheet} from "react-native";
+import styles from "./styles.js";
 import MaquetteForm from "../../../stories/components/MaquetteForm";
 
 class Accordion extends React.Component {
@@ -26,13 +27,13 @@ class Accordion extends React.Component {
 
     getItem = (id, isOpen) => {
         return <View key={`accordionItem${id}`}>
-            <View style={style.buttonsContainerAccordion}>
-                <Button style={style.buttonContent} onPress={() => this.openCloseItem(id)}>
-                    <Text style={style.textColor}>Тут буде назва обраної породи</Text>
-                    <Icon style={style.textColor} name={(isOpen) ? "arrow-dropup" : "arrow-dropdown"}/>
+            <View style={styles.buttonsContainerAccordion}>
+                <Button style={styles.buttonContent} onPress={() => this.openCloseItem(id)}>
+                    <Text style={styles.textColor}>Тут буде назва обраної породи</Text>
+                    <Icon style={styles.textColor} name={(isOpen) ? "arrow-dropup" : "arrow-dropdown"}/>
                 </Button>
-                <Button style={style.buttonDelete} onPress={() => this.deleteItem(id)}>
-                    <Icon style={style.deleteIcon} name={"trash"}/>
+                <Button style={styles.buttonDelete} onPress={() => this.deleteItem(id)}>
+                    <Icon style={styles.deleteIcon} name={"trash"}/>
                 </Button>
             </View>
             {(isOpen) ? <View>
@@ -62,82 +63,14 @@ class Accordion extends React.Component {
     };
 
     render() {
-        return <View style={style.containerAcordion}>
-            <View style={style.buttonsContainer}>
-                <Text style={style.textAddButton}>Порода</Text>
-                <Button style={style.addButton} onPress={() => this.addItem()}><Icon style={style.textColor} name={"add-circle"}/></Button>
+        return <View style={styles.containerAcordion}>
+            <View style={styles.buttonsContainer}>
+                <Text style={styles.textAddButton}>Порода</Text>
+                <Button style={styles.addButton} onPress={() => this.addItem()}><Icon style={styles.textColor} name={"add-circle"}/></Button>
             </View>
             {this.state.items.map((item) => (item) ? this.getItem(item.id, item.isOpen) : null)}
         </View>;
     }
 }
-
-const style = StyleSheet.create({
-    containerAcordion: {
-        borderWidth: 2,
-        borderRadius: 8,
-        borderColor: "#ddd",
-        width: "96%",
-        marginLeft: 5,
-        marginBottom: 15
-    },
-    buttonsContainer: {
-        flexDirection: "row",
-        width: "100%",
-        justifyContent: "space-between",
-        backgroundColor: "#ddd",
-        alignItems: "center",
-    },
-    buttonsContainerAccordion: {
-        flexDirection: "row",
-        width: "100%",
-        justifyContent: "space-between",
-        backgroundColor: "#ddd",
-        alignItems: "center",
-        marginTop: 5,
-    },
-    buttonContent: {
-        width: "90%",
-        backgroundColor: "#fff",
-        borderColor: "#555",
-        borderWidth: 1,
-        paddingRight: 10
-    },
-    buttonDelete: {
-        paddingTop: 0,
-        paddingBottom: 0,
-        backgroundColor: "#ddd",
-        borderWidth: 1,
-        alignItems: "center",
-        borderColor: "transparent",
-        width: "10%",
-        justifyContent: "center"
-    },
-    deleteIcon: {
-        color: "#000",
-        marginLeft: 0,
-        marginRight: 0,
-    },
-    addButton: {
-        paddingTop: 0,
-        paddingBottom: 0,
-        backgroundColor: "#ddd",
-        borderWidth: 1,
-        alignItems: "center",
-        borderColor: "transparent",
-        width: "10%",
-        justifyContent: "center"
-    },
-    textAddButton: {
-    //   width: "90%",
-      marginLeft: 10
-    },
-    textColor: {
-        color: "#000",
-        marginRight: 0,
-        marginLeft: 0,
-        // margin: "auto"
-    },
-});
 
 export default Accordion;
